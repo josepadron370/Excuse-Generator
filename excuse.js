350270122e5f16df1c77f9155da884c2b68c3f9b
josepadron370 <josepadron370@gmail.com>
2019-07-15 16:02:34 +0000

    first commit

---
+++ excuse.js
@@ -0,0 +1,5 @@
+let who = ['the dog ','my grandma ','his turtle ','my bird '];
+let what = ['eat ','pissed ','crushed ','broke '];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+let sentence = who[1] + what[1] + when[1];
+document.body.querySelector("#sentence").innerHTML=sentence;
